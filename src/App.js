0b2834968ec5e5787ec1bcd1f631b73182a4f4b4
Eduardo <eduardoaceves99@gmail.com>
2022-05-07 10:46:49 -0500

    Agregando homepage

--- src/App.js
+++ src/App.js
@@ -1,72 +1,22 @@
 import React from 'react';
-import './App.css';
+// This example's for browser use, so I'm using `BrowserRouter`.
+// The are other routers for other environments, though.
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
-function Todo({ todo, index, completeTodo, removeTodo }) {
+// Your components.
+import Todo from './Todo.js';
+
+export default function App () {
 	return (
-		<div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
-		{todo.text}
-		<div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-		<button onClick={() => removeTodo(index)}>x</button>
-      </div>
-		</div>
-	);
-};
-function TodoForm({ addTodo }) {
-	const [value, setValue] = React.useState("");
-	const handleSubmit = e => {
-		e.preventDefault();
-		if (!value) return;
-		addTodo(value);
-		setValue("");
-	};
-	return(
-		<form onSubmit={handleSubmit}>
-		<input
-		type="text"
-		className="input"
-		value={value}
-		onChange={e => setValue(e.target.value)}
-		/>
-		</form>
-	);
-}
-function App() {
-	const [todos, setTodos] = React.useState([
-		{ text: "Learn about React", isCompleted:false},
-		{ text: "Meet friend for lunch", isCompleted: false},
-		{ text: "Build really cool todo app", isCompleted:false}
-	]);
-	const addTodo = text => {
-		const newTodos = [...todos, { text }];
-		setTodos(newTodos);
-	};
-	const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = !newTodos[index].isCompleted;
-    setTodos(newTodos);
-  };
-	const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-	return (
-		<div className="App">
-		<div className="todo-list">
-		{todos.map((todo, index) => (
-			<Todo
-			key={index}
-			index={index}
-			todo={todo}
-			completeTodo={completeTodo}
-			removeTodo={removeTodo}
-			/>
-		))}
-		<TodoForm addTodo={addTodo} />
-		</div>
-		</div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/todo" element={<Todo />} />
+					<Route path="/" element={<HomePage />} />
+				</Routes>
+			</BrowserRouter>
 	);
 }
 
-export default App;
+function HomePage() {
+	return(<div>Hola mundo</div>);
+}
